e97229bd51f64d13308c9f4eb726c38b555953c7
RetroSabian <srajbully@retrorabbit.co.za>
2019-02-13 10:47:47 +0200

    Made Duration monetary

--- Know-Your-Nation-Speedy/Know-Your-Nation-Speedy/Models/Membership.cs
+++ Know-Your-Nation-Speedy/Know-Your-Nation-Speedy/Models/Membership.cs
@@ -13,7 +13,7 @@ namespace Know_Your_Nation_Speedy.Models
 
         public int Id { get; set; }
         public string Type { get; set; }
-        public DateTime Duration { get; set; }
+        public int Duration { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public bool AllowArticle { get; set; } = false;
